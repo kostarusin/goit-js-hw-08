b0cf2bb1c9b73084e1b00ebc044bdb3fd2b929f1
Kostyantyn <kosta.rusin@gmail.com>
2023-06-01 16:09:10 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+fillInputSavedData();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const email = refs.form.querySelector('input').value;
+  const message = refs.form.querySelector('textarea').value;
+
+  const CurrentFormState = {
+    email: email,
+    message: message,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(CurrentFormState));
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.clear();
+}
+
+function fillInputSavedData() {
+  const savedInputData = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (savedInputData) {
+    refs.form.querySelector('input').value = savedInputData.email;
+    refs.form.querySelector('textarea').value = savedInputData.message;
+  }
+}
