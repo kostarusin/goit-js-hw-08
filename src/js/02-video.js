dbbb8eb2400b71ab84436e02fc03c64eaab6e5be
Kostyantyn <kosta.rusin@gmail.com>
2023-06-01 11:35:01 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}, 1000));
+
+const lastCurrentTime = localStorage.getItem('videoplayer-current-time');
+const playCurrentTime = JSON.parse(lastCurrentTime);
+
+player
+  .setCurrentTime(playCurrentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
